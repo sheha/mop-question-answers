4d334148f803008fe2c553f18ffdc3c1b4abb2d7
sheha <sheha>
2019-03-28 02:05:49 +0100

    inital boilerplate

---
+++ frontend/src/components/about.js
@@ -0,0 +1,24 @@
+// Imports
+import React, { Component } from 'react'
+
+class About extends Component {
+  render () {
+    return (
+      <section>
+        <h2><span role="img" aria-label="about">ℹ</span>️ About</h2>
+
+        <br/>
+
+        <p>Github Repo: <a href="https://github.com/atulmy/node-express-react-redux-zwitter" target="_blank"
+                           rel="noopener noreferrer">https://github.com/atulmy/node-express-react-redux-zwitter</a></p>
+
+        <br/>
+
+        <p>Twitter: <a href="https://twitter.com/atulmy" target="_blank"
+                       rel="noopener noreferrer">https://twitter.com/atulmy</a></p>
+      </section>
+    )
+  }
+}
+
+export default About
