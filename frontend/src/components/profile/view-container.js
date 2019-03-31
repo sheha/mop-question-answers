841abd40314dc7fded413c33e26a6e58da71c06f
sheha <i.sheeha@gmail.com>
2019-03-31 09:29:08 +0200

    profile page

---
+++ frontend/src/components/profile/view-container.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Paper from '@material-ui/core/Paper';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import AddressForm from './AddressForm';
+import PaymentForm from './PaymentForm';
+import Review from './Review';
+
+const styles = theme => ({
+
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+      width: 600,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3,
+    },
+  },
+  stepper: {
+    padding: `${theme.spacing.unit * 3}px 0 ${theme.spacing.unit * 5}px`,
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    marginTop: theme.spacing.unit * 3,
+    marginLeft: theme.spacing.unit,
+  },
+});
+
+const tabs = ['Personal', 'My Questions', 'Change Password'];
+
+function getTabContent(step) {
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 1:
+      return <PaymentForm />;
+    case 2:
+      return <Review />;
+    default:
+      throw new Error('Unknown step');
+  }
+}
+
+class Checkout extends React.Component {
+  state = {
+    activeTab: 0,
+  };
+
+  handleTab = (event,value) => {
+    this.setState(state => ({
+      activeTab: valye,
+    }));
+  };
+
+  handleBack = () => {
+    this.setState(state => ({
+      activeTab: state.activeTab - 1,
+    }));
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeTab: 0,
+    });
+  };
+
+  render() {
+    const { classes, user } = this.props;
+    const { activeTab } = this.state;
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Typography component="h1" variant="h4" align="center">
+              Profile Page
+            </Typography>
+            <Tabs
+          value={this.state.activeTab}
+          onChange={this.handleTab}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab label="Personal" />
+          <Tab label="My Questions" />
+          <Tab label="Change Pass" />
+        </Tabs>
+            <React.Fragment>
+              {activeTab === tabs.length ? (
+                <React.Fragment>
+                  <Typography variant="h5" gutterBottom>
+                    Your password has been changed.
+                  </Typography>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  {getTabContent(activeTab)}
+                  <div className={classes.buttons}>
+                    {activeTab !== 0 && (
+                      <Button onClick={this.handleBack} className={classes.button}>
+                        Back
+                      </Button>
+                    )}
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.handleNext}
+                      className={classes.button}
+                    >
+                      {activeTab === steps.length - 1 ? 'Place order' : 'Next'}
+                    </Button>
+                  </div>
+                </React.Fragment>
+              )}
+            </React.Fragment>
+          </Paper>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+Checkout.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Checkout);
